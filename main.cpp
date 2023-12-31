14bcaaebeb4b0c467ebdf664b581f40ec7f90980
floxo115 <flojak@gmx.net>
2023-12-31 15:20:09 +0100

    added paper dataset

--- main.cpp
+++ main.cpp
@@ -3,7 +3,12 @@
 //
 
 #include "library.h"
+#include "paper_data.h"
+#include <iostream>
 
 int main(){
     hello();
+    std::cout << paper_data_x << std::endl;
+    std::cout << paper_data_y << std::endl;
+
 }
